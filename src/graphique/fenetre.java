c8ec5a4d0d7efd48a0e5fefc3535e98daededfaa
mvincent <mickaelvincent70@outlook.fr>
2016-12-17 11:46:43 +0100

    Ajouts de méthodes sur la classe fenetre Création d'une fenetre dans main

--- src/graphique/fenetre.java
+++ src/graphique/fenetre.java
@@ -1,5 +1,23 @@
 package graphique;
 
-public class fenetre {
+import javax.swing.*;
+import java.awt.*;
 
+public class fenetre extends JFrame{
+    public fenetre(){
+        createWidget();
+        setVisible(true);
+
+    }
+
+    private void createWidget() {
+        JRadioButton rb1 = new JRadioButton("Musee", false);
+        JRadioButton rb2 = new JRadioButton("Monuments Historique", false);
+    }
+    private void setListener(){
+
+    }
+    private void setWindowParameters(){
+
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,14 @@
 package main;
 
+import graphique.fenetre;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("MIKA EST MAUVAIS LOL !");
 		System.out.println("OUAIS TG GROS CON!");
+        fenetre window = new fenetre();
 	}
 
 }
