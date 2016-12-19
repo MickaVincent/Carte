5cd66ddb5c3979fa0ff877bb98ca213e75214eed
Hamor <vinz.soto@gmail.com>
2016-12-19 18:24:08 +0100

    ajout de commentaire pour la classe Parse

--- src/csvToArray/Parse.java
+++ src/csvToArray/Parse.java
@@ -11,24 +11,33 @@ import java.util.Scanner;
 
 public class Parse {
     private static final char DEFAULT_SEPARATOR = '\t';
-    private static final char DEFAULT_NEW_LINE = '\n';
+    //private static final char DEFAULT_NEW_LINE = '\n';
     //private static String str;
+
+    //parseur du fichier scv en entier
     private Scanner fileScan;
+
+    //parseur de ligne
     private Scanner sc;
 
     //Constructeur
     //Construit un "Parse" avec un parseur sur le fichier fournit en parametre
     public Parse(File f){
         try {
+            //fileScan parsera sur tout le fichier
             fileScan = new Scanner(f);
+
             //On "passe" la première ligne de description
             fileScan.nextLine();
-            sc = new Scanner(fileScan.nextLine()).useDelimiter("\t");
+
+            //sc n'aura qu'une seule ligne a parsé avec la tabulation comme séparateur
+            sc = new Scanner(fileScan.nextLine()).useDelimiter("DEFAULT_SEPARATOR");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    //methode pour passée à la tabulation suivante, renvoie une chaine vide si la ligne est finie
     public String nextField(){
         try {
             return sc.next();
@@ -37,14 +46,19 @@ public class Parse {
         }
     }
 
+    //sert à savoir si le fichier en lecture est fini ou non
     public boolean isFinished(){
         return (!fileScan.hasNextLine());
     }
 
+    //permet à sc de lire la ligne suivante fournie par fileScan, on doit fermer sc avant de lui réaffecter une
+    //nouvelle ligne, on reprécise le délimiteur
     public void nextLine(){
         sc.close();
-        sc = new Scanner(fileScan.nextLine()).useDelimiter("\t");
+        sc = new Scanner(fileScan.nextLine()).useDelimiter("DEFAULT_SEPARATOR");
     }
+
+    //ferme tous les scanners ouvert
     public void close(){
         sc.close();
         fileScan.close();
