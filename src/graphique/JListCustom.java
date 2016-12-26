b247ccd5feb9420c29a00e21c169af12ac6dea51
Hamor <vinz.soto@gmail.com>
2016-12-26 20:42:51 +0100

    surlignement lors de selection dans la classe JListCustom

--- src/graphique/JListCustom.java
+++ src/graphique/JListCustom.java
@@ -24,13 +24,16 @@ public class JListCustom extends JLabel implements ListCellRenderer<PointInteret
 
         if (isSelected) {
             setBackground(list.getSelectionBackground());
+//            setBackground(new Color(64, 180, 220));
             setForeground(list.getSelectionForeground());
         } else {
             setBackground(list.getBackground());
             setForeground(list.getForeground());
         }
-
+        setEnabled(list.isEnabled());
+        setFont(list.getFont());
         setText(name);
+        setOpaque(true);
         return this;
     }
 }
