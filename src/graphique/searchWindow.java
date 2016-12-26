259e520bb8d9c342b5b976d2c446a1a7d7662c9c
MickaelVincent <mickaelvincent70@outlook.fr>
2016-12-26 21:14:49 +0100

    Ajout des Monuments Historiques dans l'IHM

--- src/graphique/searchWindow.java
+++ src/graphique/searchWindow.java
@@ -12,18 +12,25 @@ import java.util.*;
  * Created by mvincent on 20/12/16.
  */
 public class searchWindow extends JDialog {
+    private String[] tabMonuments = {"Musee", "Monument Historique", "Les deux"};
     private Dimension screenSize;
     private BoxLayout layout = new BoxLayout(this, 0);
     private JButton b1, b2;
-    private JComboBox comboBox;
+    private JLabel lbl1;
+    private JComboBox<String> comboBox;
     public searchWindow(){
         setSize(350, 250);
+        this.setLayout(new FlowLayout());
         setModal(true);
         setResizable(false);
         setTitle("Recherche Avancée");
         screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(screenSize.width/2-this.getSize().width/2, screenSize.height/2-this.getSize().height/2);
 
+        lbl1 = new JLabel("Type de structure recherché :");
+        comboBox = new JComboBox(tabMonuments);
+        this.add(lbl1);
+        this.add(comboBox);
         /*DefaultListModel<String> model = new DefaultListModel<String>();
 
         java.util.List<Musee> maListe = MonumentList.getMuseesList();
